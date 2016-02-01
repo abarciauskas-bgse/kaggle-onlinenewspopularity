4e771e6c4521c31897634056f5db5449ed9bb15b
zsuzsaholler <holler.zsuzsa@gmail.com>
2016-02-01 14:33:45 +0100

    Added .R file with useful function definitions.

---
+++ functions.R
@@ -0,0 +1,22 @@
+validation <- function( data , percentage ){
+  nobs <- nrow(data)
+  validation.indices <- sample(nobs, percentage*nobs)
+  train.indices <- setdiff(1:nobs, validation.indices)
+  data.validation <- data[validation.indices,]
+  data.train <- data[train.indices,]
+  return(list(
+    validation = data.validation,
+    train = data.train
+  ))
+}
+
+success.rate <- function(predictions, actual) {
+  errors <- 0
+  # count number of mistakes
+  for (i in 1:length(actual)) {
+    if (predictions[i] != actual[i]) {
+      errors <- errors + 1
+    }
+  }
+  return (1 - errors/length(actual))
+}
